302c3aca1e8c637a81961d4bb81cb5e72c78fbb5
Adanurk <gumussoykbr@gmail.com>
2022-09-06 12:43:15 +0200

    firebase js created

---
+++ src/auth/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBAXUFc8t5Yl5rMmJiVkolo7VOo9A0PiGM",
+    authDomain: "movie-app-93dd8.firebaseapp.com",
+    projectId: "movie-app-93dd8",
+    storageBucket: "movie-app-93dd8.appspot.com",
+    messagingSenderId: "337729760577",
+    appId: "1:337729760577:web:eed2456671e4a6e69a85c0"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
