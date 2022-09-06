8d0f3e5615be0b2159c0872bb9bab6b04e84e085
Adanurk <gumussoykbr@gmail.com>
2022-09-06 12:24:37 +0200

    register edited

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,11 +7,10 @@ const Register = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
-  const handleSubmit = () => {
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(firstName);
   }
-
-
 
   return (
     <div className='d-flex justify-content-center flex-wrap'>
@@ -20,7 +19,7 @@ const Register = () => {
       </div>
       <div className='register-form'>
         <h1 className='form-title display-3'>Register</h1>
-        <form id='register'>
+        <form id='register' onSubmit={handleSubmit}>
           <div className='mb-3'>
             <label htmlFor="first-name" className='form-label'>First Name</label>
             <input type="text" className='form-control' id='first-name' placeholder='Enter your first name' onChange={(e) => setFirstName(e.target.value)}/>
@@ -37,7 +36,7 @@ const Register = () => {
             <label htmlFor="password" className='form-label'>Password</label>
             <input type="password" className='form-control' id='password' placeholder='Enter your password' onChange={(e) => setPassword(e.target.value)}/>
           </div>
-          <input type="button" className="btn btn-primary form-control" value="Register" onClick={handleSubmit}/>
+          <input type="submit" className="btn btn-primary form-control" value="Register"/>
         </form>
 
       </div>
