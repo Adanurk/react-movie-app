f71241256c5f6102f3869fe23db0932158cb409e
Adanurk <gumussoykbr@gmail.com>
2022-09-06 12:19:31 +0200

    register done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import myImg from "./../cinema.avif";
 
 const Register = () => {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = () => {
+    
+  }
+
+
+
   return (
-    <div>Register</div>
+    <div className='d-flex justify-content-center flex-wrap'>
+      <div className='form-image'>
+        <img src={myImg} alt="img"/>
+      </div>
+      <div className='register-form'>
+        <h1 className='form-title display-3'>Register</h1>
+        <form id='register'>
+          <div className='mb-3'>
+            <label htmlFor="first-name" className='form-label'>First Name</label>
+            <input type="text" className='form-control' id='first-name' placeholder='Enter your first name' onChange={(e) => setFirstName(e.target.value)}/>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor="last-name" className='form-label'>Last Name</label>
+            <input type="text" className='form-control' id='last-name' placeholder='Enter your last name' onChange={(e) => setLastName(e.target.value)}/>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor="email" className='form-label'>Email</label>
+            <input type="email" className='form-control' id='email' placeholder='Enter your email address' onChange={(e) => setEmail(e.target.value)}/>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor="password" className='form-label'>Password</label>
+            <input type="password" className='form-control' id='password' placeholder='Enter your password' onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <input type="button" className="btn btn-primary form-control" value="Register" onClick={handleSubmit}/>
+        </form>
+
+      </div>
+
+
+
+    </div>
   )
 }
 
