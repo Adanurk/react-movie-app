797825076f8fdef83841d487a5b53add5242087b
Adanurk <gumussoykbr@gmail.com>
2022-09-06 12:29:30 +0200

    login page done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import myImg from "./../cinema.avif";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  }
   return (
-    <div>Login</div>
+    <div className='d-flex justify-content-center flex-wrap'>
+      <div className='form-image'>
+        <img src={myImg} alt="img"/>
+      </div>
+      <div className='register-form'>
+        <h1 className='form-title display-3'>Register</h1>
+        <form id='register' onSubmit={handleSubmit}>
+          <div className='mb-3'>
+            <label htmlFor="email" className='form-label'>Email</label>
+            <input type="email" className='form-control' id='email' placeholder='Enter your email address' onChange={(e) => setEmail(e.target.value)}/>
+          </div>
+          <div className='mb-3'>
+            <label htmlFor="password" className='form-label'>Password</label>
+            <input type="password" className='form-control' id='password' placeholder='Enter your password' onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <input type="submit" className="btn btn-primary form-control" value="Login"/>
+        </form>
+        <button className='btn btn-primary form-control'>Continue with Google</button>
+      </div>
+    </div>
   )
 }
 
