84826c242009917acb19893bed2aab4241343328
Adanurk <gumussoykbr@gmail.com>
2022-09-08 10:42:19 +0200

    in progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,76 @@
-import React, { useContext,useLayoutEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { logOut, auth } from '../auth/firebase';
-import { AuthContext } from '../context/AuthContext';
-import { onAuthStateChanged } from 'firebase/auth';
+import React, { useContext, useLayoutEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { logOut, auth } from "../auth/firebase";
+import { AuthContext } from "../context/AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Navbar = () => {
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-  const {currentUser, setCurrentUser} = useContext(AuthContext);
+  const { currentUser, setCurrentUser } = useContext(AuthContext);
   console.log(currentUser);
 
-  useLayoutEffect(()=> {
+  useLayoutEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
-        if(currentUser){
-            setCurrentUser(currentUser);
-
-        }else{
-            // user signed out
-            setCurrentUser(null);
-        }
+      if (currentUser) {
+        setCurrentUser(currentUser);
+      } else {
+        // user signed out
+        setCurrentUser(null);
+      }
     });
-    setTimeout(() => {setLoading(false)}, 1000)
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
     // userObserver(setCurrentUser)
-}, [])
-// we use it in useEffect so it works in first time we run the app
-
+  }, []);
+  // we use it in useEffect so it works in first time we run the app
 
   return (
     <div>
-      <nav className='navbar navbar-expand-lg'>
-        <div className='container-fluid'>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
           <Link to={"/"} className="navbar-brand text-white">
             <h4>Movie App</h4>
           </Link>
           <div className="d-flex text-white align-items-center">
-
-         {loading ? <div class="spinner-border" role="status">
-  <span class="sr-only"></span>
-</div> : currentUser ? <><h5 className='mb-0 text-capitalize'>{currentUser?.displayName}</h5><button className='ms-2 btn btn-outline-light' onClick={() => logOut()}>Logout</button></> : <><button className='ms-2 btn btn-outline-light' onClick={() => navigate("/login")}>Login</button><button className='ms-2 btn btn-outline-light' onClick={() => navigate("/register")}>Register</button></>
-}
+            {loading ? (
+              <div className="spinner-border" role="status">
+                <span className="sr-only"></span>
+              </div>
+            ) : currentUser ? (
+              <>
+                <h5 className="mb-0 text-capitalize">
+                  {currentUser?.displayName}
+                </h5>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => logOut()}
+                >
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </button>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate("/register")}
+                >
+                  Register
+                </button>
+              </>
+            )}
           </div>
         </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import {useEffect} from 'react'
+import axios from 'axios';
+import { getMouseEventOptions } from '@testing-library/user-event/dist/utils';
 
 const API_KEY = process.env.REACT_APP_TMDB_KEY;
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const getMovies = (API) => {
+    axios.get(API)
+          .then((res) => console.log(res.data.results))
+          .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getMovies(FEATURED_API)
+  }, [])
 
 
   return (
@@ -13,3 +24,5 @@ const Main = () => {
 }
 
 export default Main
+
+//PAGINATION ?? FOR MORE PAGES??
