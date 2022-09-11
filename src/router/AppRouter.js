69af32b812d57f788dc33d9a571a84fd99e5639e
Adanurk <gumussoykbr@gmail.com>
2022-09-11 15:58:10 +0200

    private route added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,9 +1,11 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import Register from "../pages/Register";
 import MovieDetail from "../pages/MovieDetail";
+import {currentUser} from "../context/AuthContext";
+import { useContext } from "react";
 
 const AppRouter = () => {
   return (
@@ -13,7 +15,7 @@ const AppRouter = () => {
             <Route path="/" element={<Main/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
-            <Route path="/details/:id" element={<MovieDetail />} />
+            <Route path="/details/:id" element={currentUser ? <MovieDetail /> : <Navigate to="/login" replace/>} />
         </Routes>
     </Router>
   )
